b76ebf3d3320282e81a7830d0e913561ebacbfd6
Chris Tulin <47799223+christulin@users.noreply.github.com>
2021-01-13 18:51:47 -0800

    Build start of basic seeding script

---
+++ db/seeding.js
@@ -0,0 +1,48 @@
+//need to build schema and connect to actual mongodb model in ./index.js
+const seeder = require('mongoose-seed');
+const db = "enter db location here"
+
+const generateRandomCapacity = () => {
+  return 10 * Math.floor(Math.random() * 50);
+};
+
+const generateRandomOccupancy = (max) => {
+  return Math.floor(Math.random() * max);
+};
+
+const data = [
+  {
+    'model': 'ENTER MODEL NAME HERE',
+    'documents': []
+  }
+];
+
+for (let i = 0; i < 100; i ++) {
+
+  const max = generateRandomCapacity();
+  const current = generateRandomOccupancy(max);
+
+  const workspace = {
+    id: i,
+    isAvailable: max > current ? true : false,
+    maxCapacity: max,
+    currentCapacity: current
+  }
+
+  data[0].documents.push(workspace);
+
+}
+
+seeder.connect(db, () => {
+
+  seeder.loadModels(['./index.js']);
+
+  seeder.clearModels(['ENTER MODEL NAME'], () => {
+
+    seeder.populateModels(data, () => {
+      seeder.disconnect();
+    });
+
+  });
+
+});
