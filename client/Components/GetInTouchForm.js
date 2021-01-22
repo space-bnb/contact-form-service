1a4a3f16e87399a465e8893b244a4e817ec4e740
Chris Tulin <47799223+christulin@users.noreply.github.com>
2021-01-21 19:19:12 -0800

    Save progress on error messages

--- client/Components/GetInTouchForm.js
+++ client/Components/GetInTouchForm.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const GetInTouchForm = ( { handleFormChange }) => {
 
-  const handleFormFieldClick = (e) => {
+  const [isHidden, toggleHidden] = useState(true);
+
+  const handleFormFieldFocus = (e) => {
 
     const field = e.target.parentElement;
     field.classList.add('focus');
+
+  }
+
+  const handleFieldBlur = (e) => {
+
+    const field = e.target.parentElement;
+
+    //can eventually add other helper functions here to do some *basic* front end validation
+    if (!e.target.value) {
+
+      if (field.classList.contains('complete')) field.classList.remove('complete');
+
+      field.classList.add('error');
+      toggleHidden(false);
+
+    } else {
+
+      if (field.classList.contains('error')) field.classList.remove('error');
+
+      field.classList.add('complete');
+      toggleHidden(true);
+
+    }
+
 
   }
 
@@ -19,29 +45,56 @@ const GetInTouchForm = ( { handleFormChange }) => {
         <div className="form-item">
           <div className="field" >
             <label htmlFor="fullName">Full name</label>
-            <input id="fullName" name="fullName"  type="text" required onClick={(e) => {handleFormFieldClick(e)}}/>
+            <input
+              id="fullName"
+              name="fullName"
+              type="text"
+              required
+              onFocus={(e) => {handleFormFieldFocus(e)}}
+              onBlur={(e) => {handleFieldBlur(e)}}/>
+              <div className="error-message" hidden={isHidden}>First and last, please.</div>
           </div>
         </div>
         <div className="form-item">
           <div className="field">
             <label htmlFor="email">Work Email</label>
-            <input id="email" name="email" type="email" required onClick={(e) => {handleFormFieldClick(e)}}/>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              required
+              onClick={(e) => {handleFormFieldFocus(e)}}
+              onBlur={(e) => {handleFieldBlur(e)}}/>
           </div>
         </div>
         <div className="form-item">
           <div className="field">
             <label htmlFor="phone">Phone Number</label>
-            <input id="phone" name="phone" type="phone" required onClick={(e) => {handleFormFieldClick(e)}}/>
+            <input
+              id="phone"
+              name="phone"
+              type="phone"
+              required
+              onClick={(e) => {handleFormFieldFocus(e)}}
+              onBlur={(e) => {handleFieldBlur(e)}}/>
           </div>
         </div>
         <div className="form-item cell">
           <label htmlFor="moveInDateBtn">Move-in date</label>
           <button id="moveInDataBtn" type="button">Jan 21</button>
-          <input id="moveInDate" name="moveInDate" hidden type="text" />
+          <input
+            id="moveInDate"
+            name="moveInDate"
+            hidden
+            type="text" />
         </div>
         <div className="form-item cell">
           <label htmlFor="desiredCapacity">Number of People</label>
-          <input id="desiredCapacity" type="number" value="1" required></input>
+          <input
+            id="desiredCapacity"
+            type="number"
+            value="1"
+            required></input>
           <button>-</button>
           <button>+</button>
         </div>
