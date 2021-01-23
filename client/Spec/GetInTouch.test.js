4408b6b6b684229894f5d4737b1f3185b7c8431b
Chris Tulin <47799223+christulin@users.noreply.github.com>
2021-01-23 13:34:55 -0800

    committing progress

--- client/Spec/GetInTouch.test.js
+++ client/Spec/GetInTouch.test.js
@@ -1,9 +1,18 @@
-import { TestScheduler } from 'jest';
+// import { TestScheduler } from 'jest';
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+
+
 import React from 'react';
 import renderer from 'react-test-renderer';
 import GetInTouchForm from '../Components/GetInTouchForm';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
 
-test('We have a form!', () => {
+test('Form loads correctly', () => {
   const component = renderer.create(
     <GetInTouchForm />
   );
@@ -11,4 +20,19 @@ test('We have a form!', () => {
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 
-})
+});
+
+test('form fields work properly', () => {
+
+  const wrapper = shallow(<GetInTouchForm />);
+  const onButtonClick = sinon.spy(function() {});
+
+  const items = wrapper.find('div.form-item')
+  expect(items.length).toEqual(5);
+
+  wrapper.find('label').at(0).simulate('click');
+  wrapper.find('label').at(1).simulate('click');
+  const firstNameError = wrapper.find('div.error-message');
+  expect(firstNameError).to.not.have.property('hidden');
+
+});
