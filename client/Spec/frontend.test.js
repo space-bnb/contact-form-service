d03e58b31829158cc4c5d55de3c0b03642c51936
Chris Tulin <47799223+christulin@users.noreply.github.com>
2021-01-25 20:21:15 -0800

    remove unfinished tests

--- client/Spec/frontend.test.js
+++ client/Spec/frontend.test.js
@@ -3,7 +3,7 @@ import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import React from 'react';
 import renderer from 'react-test-renderer';
 import GetInTouchForm from '../Components/GetInTouchForm';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import sinon from 'sinon';
 
 Enzyme.configure({ adapter: new Adapter() });
@@ -27,15 +27,6 @@ describe('Get In Touch Form', () => {
 
     const items = wrapper.find('div.form-item')
     expect(items.length).toEqual(5);
-
-    // const wrapperTwo = mount(<GetInTouchForm />)
-
-    // wrapperTwo.find('label').at(0).simulate('click');
-    // wrapperTwo.find('label').at(1).simulate('click');
-    // const firstNameError = wrapperTwo.find('.error-message');
-
-
-    // expect(firstNameError).to.not.have.property('hidden');
 
   });
 
